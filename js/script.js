6d99db4a190c09fea2cde78896e277d1b931cc71
Ivon Martinez <ivon.mrtnz@gmail.com>
2024-09-04 00:25:56 +0100

    added cursor trail home page

--- js/script.js
+++ js/script.js
@@ -1,38 +1,175 @@
-    $(document).ready(function() {
 
-        $('#slides').superslides({
 
-            animation:'fade',
-            play: 5000,
-            pagination: false
+// const svg = document.querySelector('svg.trail')
+// const path = svg.querySelector('path')
 
-        });
 
-    });
+// let points = []
+// let segments = 20
+// let mouse = {
+//   x: 0,
+//   y: 0,
+// }
 
-    function openNav() {
-        document.getElementById("myNav").style.height = "100%";
-        document.getElementById("title").style.display = "none";
+// const move = (event) => {
+//   const x = event.clientX
+//   const y = event.clientY
+
+//   mouse.x = x
+//   mouse.y = y
+
+//   if (points.length === 0) {
+//     for (let i = 0; i < segments; i++) {
+//       points.push({
+//         x: x,
+//         y: y,
+//       })
+//     }
+//   }
+// }
+
+// const anim = () => {
+//   let px = mouse.x
+//   let py = mouse.y
+
+//   points.forEach((p, index) => {
+//     p.x = px
+//     p.y = py
+
+//     let n = points[index + 1]
+
+//     if (n) {
+//       px = px - (p.x - n.x) * 0.3
+//       py = py - (p.y - n.y) * 0.3
+//     }
+//   })
+
+//   path.setAttribute('d', `M ${points.map((p) => `${p.x} ${p.y}`).join(` L `)}`)
+
+//   requestAnimationFrame(anim)
+// }
+
+// const resize = () => {
+//   const ww = window.innerWidth
+//   const wh = window.innerHeight
+
+//   svg.style.width = ww + 'px'
+//   svg.style.height = wh + 'px'
+//   svg.setAttribute('viewBox', `0 0 ${ww} ${wh}`)
+// }
+
+// document.addEventListener('mousemove', move)
+// window.addEventListener('resize', resize)
+
+// anim()
+// resize()
+
+
+
+
+$(document).ready(function() {
+
+  $('#slides').superslides({
+      animation:'fade',
+      play: 5000,
+      pagination: false
+  });
+});
+
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+  document.getElementById("title").style.display = "none";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+  document.getElementById("title").style.display = "inline";
+
+}
+
+ function openPage(pageName) {
+  window.open(pageName, '_self');
+ }
+
+const svg = document.querySelector('svg.trail')
+const path = svg.querySelector('path')
+
+const slides = document.getElementById('slides');
+
+let points = []
+let segments = 20
+let mouse = {
+x: 0,
+y: 0,
+}
+
+const move = (event) => {
+
+const rect = slides.getBoundingClientRect();
+
+  if (
+  event.clientX >= rect.left &&
+  event.clientX <= rect.right &&
+  event.clientY >= rect.top &&
+  event.clientY <= rect.bottom
+  ) 
+  {
+
+    const x = event.clientX
+    const y = event.clientY
+
+    mouse.x = x
+    mouse.y = y
+
+    if (points.length === 0) {
+      for (let i = 0; i < segments; i++) {
+        points.push({
+          x: x,
+          y: y,
+        })
       }
-      
-      function closeNav() {
-        document.getElementById("myNav").style.height = "0%";
-        document.getElementById("title").style.display = "inline";
+    }
+  }
 
-      }
+  else {
+  // Clear points to stop drawing outside the element
+  points = [];
+  }
+};
 
-      // function openCvPage() {
-      //   window.open('cvpage.html', '_self');
-      // }
+const anim = () => {
 
-      // document.getElementById('cvPagebutton').addEventListener('click',openCvPage);
+let px = mouse.x
+let py = mouse.y
 
-       function openPage(pageName) {
-         window.open(pageName, '_self');
-       }
+points.forEach((p, index) => {
+p.x = px
+p.y = py
 
+let n = points[index + 1]
 
-    //   function openAbout() {
-    //     document.getElementById("aboutOverlay").style.display = "block";
-    //     document.getElementById("title").style.display = "none";
-    // }
+if (n) {
+px = px - (p.x - n.x) * 0.3
+py = py - (p.y - n.y) * 0.3
+}
+})
+
+path.setAttribute('d', `M ${points.map((p) => `${p.x} ${p.y}`).join(` L `)}`)
+
+requestAnimationFrame(anim)
+}
+
+const resize = () => {
+const ww = window.innerWidth
+const wh = window.innerHeight
+
+svg.style.width = ww + 'px'
+svg.style.height = wh + 'px'
+svg.setAttribute('viewBox', `0 0 ${ww} ${wh}`)
+}
+
+document.addEventListener('mousemove', move)
+window.addEventListener('resize', resize)
+
+anim()
+resize()
